03d5b684d1e5f4b7a47da05a0e350fb4bda7fbc0
Precious <preciousnyaupane3@gmail.com>
2025-01-01 10:48:42 -0700

    Initial commit

---
+++ src/main/java/com/example/examplemod/item/ModCreativeModTabs.java
@@ -0,0 +1,32 @@
+package com.example.examplemod.item;
+
+import com.example.examplemod.ExampleMod;
+import com.example.examplemod.block.ModBlocks;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModCreativeModTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+        DeferredRegister.create(Registries.CREATIVE_MODE_TAB, ExampleMod.MODID);
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+
+    public static final RegistryObject<CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("example_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.RawRuby.get()))
+                    .title(Component.translatable("creativetab.example_tab"))
+                    .displayItems((itemDisplayParameters, output) -> {
+                        output.accept(ModItems.Ruby.get());
+                        output.accept(ModItems.RawRuby.get());
+
+                        output.accept(ModBlocks.RUBY_BLOCK.get());
+                    })
+                    .build());
+}
